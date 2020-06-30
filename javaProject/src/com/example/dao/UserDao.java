332704a12f8e1127cef5b3deeef006b4ed13171c
Fushuyue123 <2701322990@qq.com>
2020-07-01 00:49:37 +0800

    提交到本次仓库

---
+++ javaProject/src/com/example/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.example.dao;
+
+import java.util.List;
+
+import com.example.pojo.User;
+
+public class UserDao {
+
+	public void saveUser() {
+		// 保存用户
+	}
+	
+	public List<User> getUserAll() {
+		// 查询用户数据
+		return null;
+	}
+}
